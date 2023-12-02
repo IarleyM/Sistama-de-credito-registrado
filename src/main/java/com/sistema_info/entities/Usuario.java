028f339352a2f2b0fdf95285db88b8a26e9d0740
IarleyM <iarleym771@gmail.com>
2023-12-02 10:13:56 -0300

    Entity usuario

--- src/main/java/com/sistema_info/entities/Usuario.java
+++ src/main/java/com/sistema_info/entities/Usuario.java
@@ -1,8 +1,20 @@
 package com.sistema_info.entities;
 
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class Usuario {
 	Usuario(){} 
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private int idade;
@@ -30,6 +42,21 @@ public class Usuario {
 	}
 	public void setCPF(String cPF) {
 		CPF = cPF;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
